b1886223b5c3215dd343e1c9ecf7e891901c8798
Deividas Sutkus <deividas.sutkus@qa.com>
2018-02-19 19:31:37 +0000

    Escape dashes.

--- deployments.groovy
+++ deployments.groovy
@@ -14,20 +14,20 @@ folder('deployments');
       definition {
           cps {
               script('''#!/bin/bash
-                echo &apos;Spawning a slave for this job...&apos;
+                echo 'Spawning a slave for this job...'
 
-                node(&apos;docker&apos;) {
-                    stage(&apos;Clone&apos;) {
-                        git branch: &apos;${deployment.branch}&apos;, url: &apos;${deployment.repository}&apos;, changelog: false, poll: false
+                node('docker') {
+                    stage('Clone') {
+                        git branch: '${deployment.branch}', url: '${deployment.repository}', changelog: false, poll: false
                     }
 
-                    stage(&apos;Deploy&apos;) {
-                        dir(&apos;charts&apos;) {
-                            sh &apos;&apos;&apos;#!/bin/bash
-                                TILLER_PORT=$(kubectl get svc -n kube-system tiller -o jsonpath=&apos;{.spec.ports[].port}&apos;)
+                    stage('Deploy') {
+                        dir('charts') {
+                            sh '''#!/bin/bash
+                                TILLER_PORT=$(kubectl get svc -n kube-system tiller -o jsonpath='{.spec.ports[].port}')
                                 export HELM_HOST=&quot;tiller.kube-system.svc.cluster.local:$TILLER_PORT&quot;
                                 helm upgrade ${RELEASE_NAME} ./$(deployment.name} --install --set deployment.imageTag=${TAG}
-                            &apos;&apos;&apos;
+                            '''
                         }
                     }
                 }
