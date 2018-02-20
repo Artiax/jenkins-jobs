1a03be5faa435862c74d631024b342e665b9e2aa
Deividas Sutkus <deividas.sutkus@qa.com>
2018-02-20 14:59:02 +0000

    Escape dashes.

--- deployments.groovy
+++ deployments.groovy
@@ -14,7 +14,19 @@ folder('deployments');
       }
       definition {
           cps {
-              script(readFileFromWorkspace('templates/deployment.groovy'))
+              script("""
+                node('docker') {
+                    stage('Clone') {
+                        git branch: '${deployment.branch}', url: '${deployment.repository}', changelog: false, poll: false
+                    }
+
+                    stage('Deploy') {
+                        dir('charts') {
+                            sh 'helm upgrade ${RELEASE_NAME} ./${deployment.name} --install --set deployment.imageTag=${TAG} --host tiller.kube-system.svc.cluster.local:44134'
+                        }
+                    }
+                }
+              """)
               sandbox(true)
           }
       }
