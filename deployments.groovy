8a3e74361b22b158f2f57cb69b062ff4f2b646f3
Deividas Sutkus <deividas.sutkus@qa.com>
2018-02-20 16:39:00 +0000

    Jenkins jobs refactor.

--- deployments.groovy
+++ deployments.groovy
@@ -2,36 +2,38 @@ folder('deployments');
 
 [
 
-
-  [ name: 'httpd', repository: 'https://github.com/Artiax/helm.git', branch: 'master' ],
-
+  [
+    name:       'httpd',
+    repository: 'https://github.com/Artiax/helm.git',
+    branch:     'master'
+  ]
 
 ].each { Map deployment ->
   pipelineJob("deployments/${deployment.name}") {
-      parameters {
-          stringParam('RELEASE_NAME','','Release name for helm deployment. Also used as a prefix to all kubernetes components.')
-          stringParam('TAG','')
-      }
-      definition {
-          cps {
-              script("""
-                node('docker') {
-                    stage('Clone') {
-                        git branch: '${deployment.branch}', url: '${deployment.repository}', changelog: false, poll: false
-                    }
+    parameters {
+      stringParam('RELEASE_NAME','','Release name for helm deployment. Also used as a prefix to all kubernetes components.')
+      stringParam('TAG','latest','Docker image build tag to use for this deployment.')
+    }
+    definition {
+      cps {
+        sandbox(true)
+        script("""
+          node('docker') {
+            stage('Clone') {
+              git branch: '${deployment.branch}', url: '${deployment.repository}', changelog: false, poll: false
+            }
 
-                    stage('Deploy') {
-                        dir('charts') {
-                            sh '''
-                                TILLER_PORT=\$(kubectl get svc -n kube-system tiller -o jsonpath='{.spec.ports[].port}')
-                                helm upgrade \${RELEASE_NAME} ./${deployment.name} --install --set deployment.imageTag=\${TAG} --host tiller.kube-system.svc.cluster.local:\${TILLER_PORT}
-                            '''
-                        }
-                    }
-                }
-              """.stripIndent())
-              sandbox(true)
+            stage('Deploy') {
+              dir('charts') {
+                sh '''
+                  TILLER_PORT=\$(kubectl get svc -n kube-system tiller -o jsonpath='{.spec.ports[].port}')
+                  helm upgrade \${RELEASE_NAME} ./${deployment.name} --install --set deployment.imageTag=\${TAG} --host tiller.kube-system.svc.cluster.local:\${TILLER_PORT}
+                '''
+              }
+            }
           }
+        """.stripIndent())
       }
+    }
   }
 }
--- images.groovy
+++ images.groovy
@@ -2,29 +2,61 @@ folder('images');
 
 [
 
-
-  [name: 'jenkins', repository: 'https://github.com/Artiax/jenkins.git', branch: 'master'],
-  [name: 'jenkins-slave', repository: 'https://github.com/Artiax/jenkins-slave.git', branch: 'master'],
-  [name: 'httpd', repository: 'https://github.com/Artiax/httpd.git', branch: 'master'],
-
+  [
+    name:       'jenkins',
+    repository: 'https://github.com/Artiax/jenkins.git',
+    branch:     'master'
+  ],
+  [
+    name:       'jenkins-slave',
+    repository: 'https://github.com/Artiax/jenkins-slave.git',
+    branch:     'master'
+  ],
+  [
+    name:       'httpd',
+    repository: 'https://github.com/Artiax/httpd.git',
+    branch:     'master'
+  ]
 
 ].each { Map image ->
   pipelineJob("images/${image.name}") {
-      definition {
-          cpsScm {
-              scm {
-                  git {
-                      branch("${image.branch}")
-                      remote {
-                          url("${image.repository}")
-                      }
-                  }
-              }
-              scriptPath("Jenkinsfile")
+    definition {
+      cps {
+        sandbox(true)
+        script("""
+          def image;
+          def tag;
+
+          node('docker') {
+            stage('Clone') {
+              git branch: '${image.branch}', url: '${image.repository}', changelog: false, poll: false
+              commit = sh(returnStdout: true, script: "git rev-parse HEAD").trim().take(8)
+              timestamp = sh(returnStdout: true, script: "date '+%Y-%m-%d-%H%M'").trim()
+              tag = "${timestamp}-${commit}"
+            }
+
+            stage('Build') {
+              currentBuild.displayName = "${tag}"
+              image = docker.build '${image.name}'
+            }
+
+            stage('Tag') {
+
+              image.tag "${tag}"
+              image.tag "latest"
+            }
           }
+
+          timeout(time: 15, unit: 'MINUTES') {
+            try {
+              input(id: 'deploy', message: 'Deploy this image?')
+              build job: 'deployments/${image.name}', parameters: [string(name: 'RELEASE_NAME', value: 'jenkins'),string(name: 'TAG', value: "${tag}")], wait: false
+            } catch(err) {
+              currentBuild.result = 'SUCCESS'
+            }
+          }
+        """.stripIndent())
       }
-      configure {
-         it / definition / lightweight(true)
-      }
+    }
   }
 }
